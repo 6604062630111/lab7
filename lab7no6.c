5c17740428da3b9fec52c3bbbc5439c62992b116
jumponpat sakekun <152254050+6604062630111@users.noreply.github.com>
2026-02-15 17:55:19 +0700

    Add files via upload

---
+++ lab7no6.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int random100to200();
+
+int main() {
+    int M, N, i, j, k;
+    int rnum, numok;
+    int array[20][20];
+
+    printf("array sizes:");
+    scanf("%d %d", &M, &N);
+
+    if (M < 3) M = 3;
+    if (N > 19) N = 19;
+
+    srand(time(NULL));
+
+    // ??????????????????? array
+    for (j = 0; j < 20; j++) {
+        for (k = 0; k < 20; k++) {
+            array[j][k] = -1;
+        }
+    }
+
+    for (i = 0; i < M * N; i++) {
+
+        do {
+            rnum = random100to200();
+            numok = 1;
+
+            for (j = 0; j < M; j++) {
+                for (k = 0; k < N; k++) {
+                    if (array[j][k] == rnum) {
+                        numok = 0;
+                    }
+                }
+            }
+
+        } while (numok == 0);
+
+        array[i / N][i % N] = rnum;
+    }
+
+    for (j = 0; j < M; j++) {
+        for (k = 0; k < N; k++) {
+            printf("%d ", array[j][k]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+int random100to200() {
+    return (rand() % 101) + 100;
+}
+
